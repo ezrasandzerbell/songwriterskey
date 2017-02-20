62ab36898b615aaa231659589d5cc4ae94d68592
ezra sandzerbell <equilibriumarts@gmail.com>
2017-02-19 17:32:45 -0800

    fix control panel css

--- src/app/view-card/view-card.component.ts
+++ src/app/view-card/view-card.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Mcipher } from './../mcipher.model'
+import { Song } from './../song.model'
+import { SongService } from '../song.service';
+import { FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-view-card',
   templateUrl: './view-card.component.html',
-  styleUrls: ['./view-card.component.css']
+  styleUrls: ['./view-card.component.css', './view-card.component.scss'],
+  providers: [SongService]
 })
 export class ViewCardComponent implements OnInit {
+  songId: number;
+  songToDisplay;
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute,
+      private location: Location,
+      private songService: SongService
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.songId = parseInt(urlParameters['id']);
+    });
+    this.songToDisplay = this.songService.getSongById(this.songId);
   }
 
 }
